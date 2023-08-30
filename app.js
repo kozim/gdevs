a5d74da76017793366cb9dcb17b475ac9ab45b5f
{kozim} <1427205+kozim@users.noreply.github.com>
2023-08-30 15:13:28 -0400

    First commit, pushing gorgoues files

---
+++ app.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=> {
+        console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+
+/* Set width of all animated text to match container */
+let parent = document.querySelectorAll('.animate-text');
+for(let i = 0; i < parent.length; i++) {
+  parent[i].style.width = parent[i].children[0].clientWidth + "px"; 
+};
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+var fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(port);
+});
